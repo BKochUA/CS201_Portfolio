e12856a3db2972b0e7472fc69d67cd98a055511d
BKochUA <47281306+BKochUA@users.noreply.github.com>
2019-03-29 15:58:46 -0500

    Radix tree formed
    submission for Friday at 5

--- functions.h
+++ functions.h
@@ -1,6 +1,12 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <string.h>
+#include <stdbool.h>
+
 struct FoodItem
 {
     unsigned int ID;
@@ -14,12 +20,9 @@ struct FoodItem
 struct TreeNode
 {
     struct TreeNode *children[256];
-    struct TreeNode *suffixLink;
-    struct FoodItem *items[2];
+    struct FoodItem *items;
 
-    int start;
-    int *end;
-    int suffixIndex;
+    bool isLeaf;
 };
 
 typedef struct TreeNode Node;
@@ -60,59 +63,59 @@ void printFoodItem(struct FoodItem *itemToPrint)
  * energy = 200.0 * (40.0/100.0)
  * energy
 */
-void createItem(const char *data)
+struct FoodItem *createItem(const char *data)
 {
     static char tempstr[300];
     strcpy(tempstr, data);
     char *pch = tokenize(tempstr, "~");
     int count = 0;
     float tempServingWeight;
-    struct FoodItem createdItem;
+    struct FoodItem *createdItem = calloc(1, sizeof(struct FoodItem));
     while(pch != NULL)
     {
         switch(count)
         {
             case 0:
-                createdItem.ID = (unsigned int)strtod(pch, NULL);
+                createdItem->ID = (unsigned int)strtod(pch, NULL);
                 break;
             case 1:
-                strcpy(createdItem.name, pch);
+                strcpy(createdItem->name, pch);
                 break;
             case 2:
-                strcpy(createdItem.mfg, pch);
+                strcpy(createdItem->mfg, pch);
                 break;
             case 3:
-                createdItem.calories = (float)strtod(pch, NULL);
+                createdItem->calories = (float)strtod(pch, NULL);
                 break;
             case 4:
-                createdItem.carbs = (float)strtod(pch, NULL);
+                createdItem->carbs = (float)strtod(pch, NULL);
                 break;
             case 5:
-                createdItem.fat = (float)strtod(pch, NULL);
+                createdItem->fat = (float)strtod(pch, NULL);
                 break;
             case 6:
-                createdItem.protein = (float)strtod(pch, NULL);
+                createdItem->protein = (float)strtod(pch, NULL);
                 break;
             case 7:
                 tempServingWeight = (float)strtod(pch, NULL);
-                createdItem.servingWeight = tempServingWeight;
-                createdItem.calories = createdItem.calories * (tempServingWeight / 100.0f);
-                createdItem.carbs = createdItem.carbs * (tempServingWeight / 100.0f);
-                createdItem.fat = createdItem.fat * (tempServingWeight / 100.0f);
-                createdItem.protein = createdItem.protein * (tempServingWeight / 100.0f);
+                createdItem->servingWeight = tempServingWeight;
+                createdItem->calories = createdItem->calories * (tempServingWeight / 100.0f);
+                createdItem->carbs = createdItem->carbs * (tempServingWeight / 100.0f);
+                createdItem->fat = createdItem->fat * (tempServingWeight / 100.0f);
+                createdItem->protein = createdItem->protein * (tempServingWeight / 100.0f);
                 break;
             case 8:
                 if(strcmp(pch, "g") == 0)
                 {
-                    createdItem.grams = true;
-                }else { createdItem.grams = false; }
+                    createdItem->grams = true;
+                }else { createdItem->grams = false; }
                 break;
             case 9:
-                createdItem.servingSize = (float)strtod(pch, NULL);
+                createdItem->servingSize = (float)strtod(pch, NULL);
                 break;
             case 10:
                 pch[strlen(pch)-1] = 0;
-                strcpy(createdItem.servingUnits, pch);
+                strcpy(createdItem->servingUnits, pch);
                 break;
             default:
                 break;
@@ -120,8 +123,313 @@ void createItem(const char *data)
         pch = tokenize(NULL, "~");
         count++;
     }
-    printFoodItem(&createdItem);
-    printf("_____________________________________________________\n");
+    return createdItem;
+    //printFoodItem(&createdItem);
+    //printf("_____________________________________________________\n");
+}
+
+//Fast and Space Efficient Trie Searches - Phil Bagwell
+/*
+struct AMTNode
+{
+    unsigned int NodeCnt:16, Chr:16; //16 bits each
+    #ifdef AMTSMALL
+    union{
+        struct AMTNode *IndexBaseA;
+        int Value;
+    };
+    #else
+        struct AMTNode *IndexBaseA;
+        int Value;
+    #endif
+
+};
+
+struct AMTNode *AMTNext(struct AMTNode *pNode, unsigned short chr)
+{
+    struct AMTNode *pList;
+    int L,H,M,NCnt;
+    if(!(NCnt=pNode->NodeCnt)) return NULL;
+    pList = pNode->IndexBaseA;
+    if(NCnt <= 32)
+    {
+        do{
+            if(chr == pList->Chr) return pList;
+            pList++;
+        }while(--NCnt);
+        return NULL;
+    }
+
+    unsigned short Idx, Map;
+    Idx = chr>>5; //get top bits
+    chr&=0x1F; //clear top bits in chr
+    Map=((AMTBMap *)pList)[Idx].BitMap;
+}
+ */
+
+
+
+
+
+//kukuruku.co/post/radix-trees
+struct RadixNode // a structure to represent tree nodes
+{
+    //char* key;
+    int len;
+    struct RadixNode* link;
+    struct RadixNode* next;
+    char key[];
+};
+
+struct RadixNode *newRadixNode(char *x, int n)
+{
+    struct RadixNode *RN = malloc(sizeof(struct RadixNode) + n*sizeof(char));
+    RN->len = n;
+    RN->link = 0;
+    RN->next = 0;
+    //memset(RN->key, 0, n*sizeof(char));
+    //RN->key[n] = '\0';
+    strncpy(RN->key, x, n);
+    return RN;
+}
+
+int prefix(const char *x, int n, const char *key, int m) // length of the biggest common prefix of x and key strings
+{
+    for( int k=0; k<n; k++ ) {
+        if (k == m || x[k] != key[k])
+        {
+            return k;
+        }
+    }
+    return n;
+}
+
+struct RadixNode* find(struct RadixNode* t, char* x, int n) // x key search in t tree, call with n = 0
+{
+    //printf("%s", t->key);
+    if( !n ) n = (int)strlen(x)+1;
+    if( !t ) return 0;
+    int k = prefix(x,n,t->key,t->len);
+    if( k==0 ) return find(t->next,x,n); // let’s look for the child’s node
+    if( k==n ) return t;
+    if( k==t->len ) return find(t->link,x+k,n-k); // let’s look for at the child’s node
+    return 0;
+}
+
+struct RadixNode* findByPrefix(struct RadixNode *root, char* searchKey, int n)
+{
+    if( !n ) //if n is 0, assign to length of searchkey
+    {
+        n = (int)strlen(searchKey);
+    }
+    if( !root )
+    {
+        return 0;
+    }
+    int k = prefix(searchKey,n,root->key,root->len);
+    if(k==0)
+    {
+        printf("no match: %s\n", root->key);
+        return findByPrefix(root->next, searchKey, n);
+    }
+    if(k > 0 && k < n)
+    {
+        printf("almost match?: %s\n", root->key);
+        return findByPrefix(root->link, searchKey+1, n);
+    }
+    if(k==n)
+    {
+        printf("full match: %s\n", root->key);
+        return root;
+    }
+    return 0;
+}
+
+/*
+ if(k==n)
+    {
+        bool stillSearching = true;
+        while(stillSearching)
+        {
+            printf("\nfound: %s\n", root->key);
+            stillSearching = false;
+        }
+        return;
+    }
+*/
+void split(struct RadixNode* t, int k) // dividing t node according to k key symbol
+{
+    struct RadixNode* p = newRadixNode(t->key+k, t->len-k);
+    p->link = t->link;
+    t->link = p;
+    char a[k];
+    //char* a = new char[k];
+    strncpy(a,t->key,k);
+    //memset(t->key, 0, strlen(t->key));
+    //free(t->key);
+    strncpy(t->key, a, k);
+    //t->key = a;
+    t->len = k;
+}
+
+struct RadixNode* radixInsert(struct RadixNode *t, char* x, int n) // inserting x key in t tree, call with n = 0;
+{
+    int k = 0;
+    if( !n )
+    {
+        if(strlen(x) > 8) x = x + 9; //clears ID
+        n = (int)strlen(x)+1;
+    }
+    if( t == 0 )
+    {
+        return newRadixNode(x, n);
+    }
+    //printf("%d", t->len);
+    k = prefix(x,n,t->key,t->len);
+
+    if( k==0 ) t->next = radixInsert(t->next,x,n);
+    else if( k<n )
+    {
+        if( k<t->len ) // cut or not to cut?
+            split(t,k);
+        t->link = radixInsert(t->link,x+k,n-k);
+    }
+    return t;
+}
+
+
+
+
+
+//theoryofprogramming.com/2015/09/01/trie-tree-practise-spoj-dict/
+void insert2(struct TreeNode *trieTree, char *word)
+{
+    struct TreeNode *temp = trieTree;
+    char data[300];
+    strcpy(data, word);
+    word = word + 9; //clears
+    while(*word != '~')
+    {
+        //if(temp->children[*word - 'A'] == NULL)
+        if(temp->children[*word] == NULL)
+        {
+            //temp->children[*word- 'A'] = calloc(1, sizeof(struct TreeNode));
+            temp->children[*word] = calloc(1, sizeof(struct TreeNode));
+        }
+
+        //temp = temp->children[*word - 'A'];
+        temp = temp->children[*word];
+        ++word;
+        //printf("%s", word);
+    }
+
+
+    //temp->items = item;
+    //try gdb for this
+    //temp->items = calloc(1, sizeof(struct FoodItem));
+    temp->items[0] = *createItem(data);
+    //*temp->items = createItem(data);
+    temp->isLeaf = true;
+    //free(temp->children);
+}
+
+void newInsert(struct TreeNode *root, char *word)
+{
+    printf("%s" ,word);
+    char data[300];
+    strcpy(data, word);
+    word = word + 9; //clears
+    for(int i=0; word[i] != '~'; i++)
+    {
+        if(root->children[word[i]] == NULL)
+        {
+            root->children[word[i]] = calloc(1, sizeof(struct TreeNode));
+        }
+        root = root->children[word[i]];
+    }
+    root->isLeaf = true;
+    //root->items = calloc(1, sizeof(struct FoodItem));
+    root->items = createItem(data);
+}
+
+struct TreeNode *searchWord(struct TreeNode *node, char *searchWord)
+{
+    //printw("searching\n");
+    while(*searchWord != '\0')
+    {
+        //if(node->children[*searchWord - 'A'] != NULL)
+        if(node->children[*searchWord] != NULL)
+        {
+            node = node->children[*searchWord];
+            //node = node->children[*searchWord - 'A'];
+            ++searchWord;
+        }else
+        {
+            break;
+        }
+    }
+
+    if(*searchWord == '\0')
+    {
+        return node;
+    }else
+    {
+        return NULL;
+    }
+}
+
+
+void getAllItems(struct TreeNode *root, struct FoodItem *results, char *wordUtil, int pos)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    if(root->isLeaf)
+    {
+        for(int j=0; j<pos; j++)
+        {
+            results[j] = *root->items;
+            //strncpy(results[j], wordUtil, strlen(wordUtil));
+        }
+    }
+    for(int i=0; i<256; i++)
+    {
+        if(root->children[i] != NULL)
+        {
+            wordUtil[pos] = (char)i;
+            getAllItems(root->children[i], results, wordUtil, pos+1);
+        }
+    }
+}
+
+void printWord(char *str, int n, WINDOW *searchResults)
+{
+    for(int j=0; j<n; j++)
+    {
+        mvwprintw(searchResults, j+1, 1, "%s", str);
+    }
+}
+
+//www.ritambhara.in/print-all-words-in-a-trie-data-structure/
+void printAllWords(struct TreeNode *root, char *wordArray, int pos, WINDOW *searchResults)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    if(root->isLeaf)
+    {
+        printWord(wordArray, pos, searchResults);
+    }
+    for(int i=0; i<256; i++)
+    {
+        if(root->children[i] != NULL)
+        {
+            wordArray[pos] = (char)i;
+            printAllWords(root->children[i], wordArray, pos+1, searchResults);
+        }
+    }
 }
 
 #endif
--- main.c
+++ main.c
@@ -3,59 +3,26 @@
 #include <ncurses.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include "functions.h"
 
+int currentScreen = 0;
+const char *options[] = {"View Today's Log", "Search for Food", "Return to Login", "EXIT"};
+int keyPress;
+int selected = 0;
+int loopvar = 1;
+WINDOW *mainScreen, *mainMenu, *loginScreen, *searchBar, *searchResults;
+struct TreeNode *trieTree;
+struct RadixNode *radixTree;
+int day, month, year;
+char username[20];
+char searchKey[75];
+int ch;
 
-
-
-int main() {
-
-    //read all the data
-    FILE *datafile;
-    char str[300];
-    datafile = fopen("food_nutrient_db.csv", "r");
-    int count = 0;
-    while(fgets(str, sizeof(str), datafile))
-    {
-        if(count < 100)
-        {
-            createItem(str);
-        }
-        count++;
-    }
-
-    fclose(datafile);
-    free(datafile);
-
-    printf("done");
-    initscr();
-    noecho();
-    cbreak();
-    curs_set(0);
-
-
-    int sizeY, sizeX;
-    getmaxyx(stdscr, sizeY, sizeX);
-    WINDOW *mainScreen = newwin(sizeY, sizeX, 0, 0);
-    //printf("%d ", sizeY);
-    //printf("%d", sizeX);
-    box(mainScreen, 0, 0);
-
-    WINDOW *mainMenu = newwin(10, 50, (sizeY/2)-5, (sizeX/2)-25);
-    box(mainMenu, 0, 0);
-    refresh();
-    wrefresh(mainScreen);
-    wrefresh(mainMenu);
-    keypad(mainMenu, true);
-
-
-    const char *options[] = {"View Today's Log", "View Calendar", "Search for Food", "EXIT"};
-    int keyPress;
-    int selected = 0;
-    int loopvar = 1;
-
-    //get current date
-    int day, month, year;
+//get current date
+void GetCurrentDate()
+{
     time_t now;
     time(&now);
     struct tm *local = localtime(&now);
@@ -63,48 +30,351 @@ int main() {
     month = local->tm_mon + 1;
     year = local->tm_year + 1900;
 
-    while(loopvar)
-    {
-        for(int i = 0; i < 4; i++)
-        {
-            mvwprintw(mainMenu, 1, 1, "Welcome Brian, today's date is %d/%d/%d", month, day, year);
-            wattron(mainMenu, A_UNDERLINE);
-            mvwprintw(mainMenu, 3, 13, "Please select an option");
-            wattroff(mainMenu, A_UNDERLINE);
-            if(i == selected)
-            {
-                wattron(mainMenu, A_REVERSE);
-                mvwprintw(mainMenu, i+4, 25-((int)strlen(options[i])/2), options[i]);
-                wattroff(mainMenu, A_REVERSE);
-            }else
-            {
-                mvwprintw(mainMenu, i+4, 25-((int)strlen(options[i])/2), options[i]);
-            }
-        }
+}
 
-        keyPress = wgetch(mainMenu);
-        switch(keyPress)
+void freeMemory(struct TreeNode *node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+    if(node->isLeaf)
+    {
+        free(node->items);
+        free(node);
+    }
+    for(int i=0; i<256; i++)
+    {
+        if(node->children[i] != NULL)
         {
-            case KEY_UP:
-                if(selected != 0) selected--;
-                break;
-            case KEY_DOWN:
-                if(selected != 3) selected++;
-                break;
-            case 10:
-                if(selected == 3)
+            freeMemory(node->children[i]);
+        }
+    }
+}
+
+void ScreenHandler()
+{
+	switch(currentScreen)
+        {
+            case 0: //login screen
+                //this will be the login screen
+                box(loginScreen, 0, 0);
+                mvwprintw(loginScreen, 1, 1, "Please enter your username");
+                wmove(loginScreen, 2, 1);
+                if(strlen(username) == 0)
+                {
+                    wattron(loginScreen, A_UNDERLINE);
+                    mvwprintw(loginScreen, 2, 1, "                    ");
+                    wattroff(loginScreen, A_UNDERLINE);
+                }
+                wrefresh(loginScreen);
+                ch = getch();
+                if(ch == 27) //escape is pressed, exit program
                 {
                     loopvar = 0;
+                }else if(ch == 10) //enter is pressed, go to next screen
+                {
+                    currentScreen = 2;
+                    wclear(loginScreen);
+                }else if((ch > 64 && ch < 91) || (ch > 96 && ch < 123) || ch == 127 || ch == 32 || ch == 95) //this only allows letters, spaces, underscores, and backspaces through when inputting username
+                {
+                    if(ch == 127 && strlen(username) != 0) //handles backspacing
+                    {
+                        username[strlen(username)-1] = '\0';
+                        wattron(loginScreen, A_UNDERLINE);
+                        mvwprintw(loginScreen, 2, 1, "                    ");
+                        mvwprintw(loginScreen, 2, 1, "%s", username);
+                        wattroff(loginScreen, A_UNDERLINE);
+                        //mvwprintw(loginScreen, 2, (int)strlen(username), "%c", 0);
+                    }else if(strlen(username) < 20 && ch != 127) //puts characters into username string and on screen
+                    {
+                        if(ch == 32)
+                        {
+                            username[strlen(username)] = (char)95;
+                        }else {
+                            username[strlen(username)] = (char) ch;
+                        }
+                        wattron(loginScreen, A_UNDERLINE);
+                        mvwprintw(loginScreen, 2, 1, "%s", username);
+                        wattroff(loginScreen, A_UNDERLINE);
+                        //mvwprintw(loginScreen, 2, (int)strlen(username), "%c", ch);
+                    }
+                    if(strlen(username) == 20)
+                    {
+                        mvwprintw(loginScreen, 3, 1, "You have reached the character limit (20)");
+                    }else { mvwprintw(loginScreen, 3, 1, "                                         "); }
+
                 }
+                wrefresh(loginScreen);
+                break;
+            case 1: //loading screen
+                break;
+            case 2: //main menu
+                box(mainMenu, 0, 0);
+                GetCurrentDate();
+                for(int i = 0; i < 4; i++)
+                {
+                    mvwprintw(mainMenu, 1, 1, "Welcome %s, today's date is %d/%d/%d", username, month, day, year);
+                    wattron(mainMenu, A_UNDERLINE);
+                    mvwprintw(mainMenu, 3, 13, "Please select an option");
+                    wattroff(mainMenu, A_UNDERLINE);
+                    if(i == selected)
+                    {
+                        wattron(mainMenu, A_REVERSE);
+                        mvwprintw(mainMenu, i+4, 25-((int)strlen(options[i])/2), options[i]);
+                        wattroff(mainMenu, A_REVERSE);
+                    }else
+                    {
+                        mvwprintw(mainMenu, i+4, 25-((int)strlen(options[i])/2), options[i]);
+                    }
+                }
+
+                keyPress = wgetch(mainMenu);
+                switch(keyPress)
+                {
+                    case KEY_UP:
+                        if(selected != 0) selected--;
+                        break;
+                    case KEY_DOWN:
+                        if(selected != 3) selected++;
+                        break;
+                    case 10:
+                        if(selected == 0)
+                        {
+
+                        }else if(selected == 1)
+                        {
+                            currentScreen = 3;
+                            wclear(mainMenu);
+                        }else if(selected == 2)
+                        {
+                            currentScreen = 0; //sets current screen to loginscreen
+                            memset(username, 0, strlen(username)); //clears the username
+                            wclear(mainMenu);
+                        }else if(selected == 3)
+                        {
+                            loopvar = 0;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                wrefresh(mainMenu);
+                break;
+            case 3: //food search
+                box(mainMenu, 0, 0);
+                box(searchResults, 0, 0);
+                box(searchBar, 0, 0);
+                mvwprintw(mainScreen, 1, 1, "Enter the name of the food you're looking for:");
+                wrefresh(mainScreen);
+                wrefresh(searchBar);
+                wrefresh(searchResults);
+                ch = getchar();
+                if(strlen(searchKey) == 0)
+                {
+                    mvwprintw(searchBar, 1, 1, "                               ");
+                }
+                //mvwprintw(searchResults, 1, 1, "%d", ch);
+                //wrefresh(searchResults);
+                if(ch == 13)
+                {
+                    //mvwprintw(searchResults, 1, 1, "pressed enter");
+                    wrefresh(searchResults);
+                    //these 2 lines are just here so I can read the error summary from valgrind
+                    //loopvar = 0;
+                    //endwin();
+                    struct RadixNode foundNode = *find(radixTree, &searchKey[0], 0);
+                    printf("%s", foundNode.key);
+
+                    //mvwprintw(searchResults, 1, 1, foundNode->key);
+
+                    //old way, it works but the setup is waaaay too long
+                    /*
+                    struct TreeNode *temp = searchWord(trieTree, searchKey);
+                    if(temp != NULL)
+                    {
+                        bool childrenThere = false;
+
+                        for(int j = 0; j < 256; ++j)
+                        {
+                            if(temp->children[j] != NULL)
+                            {
+                                childrenThere = true;
+                                break;
+                            }
+                        }
+
+                        if(childrenThere)
+                        {
+                            //char printUtil[199];
+                            //char *printUtil;
+                            //char results[30][199]; //30 is number of strings, 199 is max length
+                            struct FoodItem results[30];
+                            char *wordUtil;
+
+                            //loopvar = 0;
+                            //endwin();
+
+                            getAllItems(temp, results, wordUtil, 0);
+                            for(int k=0; k<15; k++)
+                            {
+                                mvwprintw(searchResults, k+1, 1, "%u - %s", results[k].ID, results[k].name);
+                            }
+                            //printAllWords(temp, printUtil, 0, searchResults);
+
+                            //lexicographPrint(temp, printUtil, searchKey, searchResults);
+                        }else { mvwprintw(searchResults, 1, 1, "no match                        "); }
+                    }else { mvwprintw(searchResults, 1, 1, "no match                        "); }
+                     */
+                }else if(ch == 27)
+                {
+                    //mvwprintw(searchResults, 1, 1, "pressed enter");
+                    wrefresh(searchResults);
+                    loopvar = 0;
+                }else
+                {
+                    if(ch == 127 && strlen(searchKey) != 0) //handles backspacing
+                    {
+                        searchKey[strlen(searchKey)-1] = '\0';
+
+                        mvwprintw(searchBar, 1, 1, "                                                                                        ");
+                        mvwprintw(searchBar, 1, 1, "%s", searchKey);
+                        //mvwprintw(loginScreen, 2, (int)strlen(username), "%c", 0);
+                    }else if(strlen(searchKey) < 75 && ch != 127) //puts characters into username string and on screen
+                    {
+                        searchKey[strlen(searchKey)] = (char)toupper(ch);
+                        mvwprintw(searchBar, 1, 1, "%s", searchKey);
+                        //mvwprintw(loginScreen, 2, (int)strlen(username), "%c", ch);
+                    }
+                    if(strlen(searchKey) == 75)
+                    {
+                        //mvwprintw(searchBar, 1, 1, "You have reached the character limit (20)");
+                    }//else { //mvwprintw(searchBar, 1, 1, "                                         "); }
+                }
+                wrefresh(searchBar);
+                break;
+            case 4: //food diary
+                break;
+            case 5: //confirm exit screen
                 break;
             default:
                 break;
         }
+}
 
-        wrefresh(mainMenu);
+
+int main() {
+    //system("/usr/bin/resize -s 10 100");
+    currentScreen = 0;
+    //read all the data
+    FILE *datafile;
+    char str[300];
+    datafile = fopen("food_nutrient_db.csv", "r");
+    int count = 0;
+    //strcpy(username, "Brian");
+
+    trieTree = (struct TreeNode *) calloc(1, sizeof(struct TreeNode));
+    char nothing[] = "";
+    //radixTree = newRadixNode(nothing, 0);
+    //radixTree = radixInsert(0, nothing, 0);
+    //*radixTree = NULL;
+    while(fgets(str, sizeof(str), datafile))
+    {
+        if(count == 0)
+        {
+            //struct FoodItem *newItem;
+            //*newItem = createItem(str);
+            //insert2(trieTree, str);
+            //newInsert(trieTree, str);
+            radixTree = radixInsert(0, str, 0);
+            //printf("inserted: %s", radixTree->key);
+        }else
+        {
+            radixInsert(radixTree, str, 0);
+        }
+        count++;
+    }
+
+    fclose(datafile);
+    //free(datafile);
+
+    char *first = "BARBECUE";
+    struct RadixNode *result = 0;
+    result = findByPrefix(radixTree, first, 0);
+    if(result)
+    {
+        printf("\n%s\n" ,result->key);
+    } else{ printf("\nwooow ok then\n"); }
+
+    /* //prints all of the top level nodes
+    struct RadixNode *hmm = radixTree;
+    for(int l=0; l<1000; l++)
+    {
+        if(hmm->next)
+        {
+            hmm = hmm->next;
+            printf("%s\n", hmm->key);
+        }else
+        {
+            printf("end of alphabet %d\n", l);
+            break;
+        }
+    }*/
+
+    struct RadixNode *p = 0;
+    //p = find(radixTree, "ORIGINAL BARBECUE SAUCE~FRESH & EASY~189.0~48.65~0.0~0.0~37.0~g~2.0~Tbsp\n", 0);
+    p = find(radixTree, "ORIGINAL BARBECUE S", 0);
+    if(p)
+    {
+        printf("%s" ,p->key);
+    }
+
+
+    initscr();
+    //resizeterm(24, 80);
+    noecho();
+    cbreak();
+    curs_set(0);
+
+
+    int sizeY, sizeX;
+    getmaxyx(stdscr, sizeY, sizeX);
+    mainScreen = newwin(sizeY, sizeX, 0, 0);
+    //printf("%d ", sizeY);
+    //printf("%d", sizeX);
+    box(mainScreen, 0, 0);
+
+    mainMenu = newwin(10, 50, (sizeY/2)-5, (sizeX/2)-25);
+    refresh();
+    wrefresh(mainScreen);
+    wrefresh(mainMenu);
+    keypad(mainMenu, true);
+
+    loginScreen = newwin(10,50, (sizeY/2)-5, (sizeX/2)-25);
+    box(loginScreen, 0, 0);
+    refresh();
+    wrefresh(loginScreen);
+
+
+    searchBar = newwin(3, sizeX-2, 2, 1);
+    searchResults = newwin(sizeY-6, sizeX-2, 5, 1);
+    refresh();
+    wrefresh(searchBar);
+    wrefresh(searchResults);
+    wrefresh(mainScreen);
+
+
+    //loopvar = 0;
+    while(loopvar)
+    {
+	    ScreenHandler();
     }
     wrefresh(mainMenu);
 
+
     endwin();
+    //freeMemory(trieTree);
+    //free(trieTree);
+    printf("memory freeeed");
     return 0;
 }
